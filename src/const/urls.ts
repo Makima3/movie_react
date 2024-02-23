f18f5eff640356ea93208f11123d97eba55b158d
Makima3 <natalia.kolchuk@gmail.com>
2024-02-24 00:58:58 +0200

    base configuration of project

---
+++ src/const/urls.ts
@@ -0,0 +1,9 @@
+const baseURL = 'https://api.themoviedb.org/3'
+
+const movie = '/discover/movie'
+const genre = '/genre/movie/list'
+const poster = 'https://image.tmdb.org/t/p/w500'
+
+const urls = {
+
+}
