bb10bec27ccd62313d7e2c40fac38f43a7b4c137
Makima3 <natalia.kolchuk@gmail.com>
2024-02-27 23:06:56 +0200

    styling

--- src/Components/MovieContainer/MovieContain/OneMovie.tsx
+++ src/Components/MovieContainer/MovieContain/OneMovie.tsx
@@ -1,19 +1,24 @@
 import {FC} from "react";
 
-
+import css from './OneMovie.module.css'
 import {IMovie} from "../../../interfaces";
 import {urls} from "../../../const";
 interface IProps {
 movie: IMovie
 }
 export const OneMovie: FC<IProps> = ({movie}) => {
-    const { title,poster_path, vote_average} = movie
+    const { title,poster_path, vote_average, overview} = movie
 
     return (
-        <div>
-            <img src={`${urls.poster}/${poster_path}`} alt={title}/>
-            <div>{title}</div>
-            <div>{vote_average}</div>
+        <div className={css.Card}>
+            <div className={css.PosterImage}>
+                <img src={`${urls.poster}/${poster_path}`} alt={title}/>
+            </div>
+            <div className={css.Description}>
+                <h2>{title}</h2>
+                <p>{overview}</p>
+                <p>{vote_average}</p>
+            </div>
         </div>
     );
 };
