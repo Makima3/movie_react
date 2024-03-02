a37f7f8716812eb629bb24d1908a2e3b1e82a2f1
Makima3 <natalia.kolchuk@gmail.com>
2024-03-02 14:54:15 +0200

    movie info

--- src/const/urls.ts
+++ src/const/urls.ts
@@ -4,14 +4,19 @@ const genre = '/genre/movie/list';
 const movies = '/discover/movie';
 const poster = 'https://image.tmdb.org/t/p/w300/'
 const noPoster = '/noPoster.jpg'
+const movie = '/movie'
 const actors = '/movie'
-
 
 const urls = {
     genre,
     movies,
     poster,
-    actors
+    movie: {
+        byId:(id: string)=> `${movie}/${id}`
+    },
+    actors:{
+        byId: (id: string) => `${actors}/${id}/credits`
+    }
 }
 
 export {
